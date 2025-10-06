cc687613194310d3c876c973a41d5310af397059
Gerell1225 <dorjgerelchuluun@gmail.com>
2025-10-06 17:46:45 +0800

    fix text

--- src/app/launch/page.tsx
+++ src/app/launch/page.tsx
@@ -145,7 +145,7 @@ export default function LaunchPage() {
               },
             }}
           >
-            Вэбсайтыг асаах
+            Вэбсайт асаах
           </Button>
 
           <Typography
